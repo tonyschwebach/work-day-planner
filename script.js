91bb0c38f7aeb85a365a55e3fe63b0eb6170c8ff
T Schway <tony.schwebach@gmail.com>
2020-12-16 08:58:42 -0500

    psuedo code script.js

--- script.js
+++ script.js
@@ -1,16 +1,57 @@
-console.log("yo world");
+// wait for page to load before running JS code 
+$(document).ready(function(){
+  console.log("yo world");
+
+  // DOM VARIABLES
+  // var currentDayEl= $("#currentDay");
+
+  // JAVASCRIPT VARIABLES
+  // businessHours = []; //use military time for comparison with moment.js
+
+  // GIVEN I am using a daily planner to create a schedule
+  // WHEN I open the planner
+  // THEN the current day is displayed at the top of the calendar
+  // use moment to set current day in header
+  // currentDayEl.text(moment.js);
+
+  // WHEN I scroll down
+  // THEN I am presented with time blocks for standard business hours
+  // for loop over business hours to append children to container div
+    // create form element
+    // add bootstrap classes for formatting
+    // append to container div
 
 
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours
-// WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
-// WHEN I click into a time block
-// THEN I can enter an event
-// WHEN I click the save button for that time block
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
+  // WHEN I view the time blocks for that day
+  // THEN each time block is color-coded to indicate whether it is in the past, present, or future
+  // use moment.js to get time
+  // for loop over each time block
+  // if business hour < current hour, set class to past
+  // else if business hour === current hour, set class to current
+  // else set class to future
+  
+  // WHEN I click into a time block
+  // THEN I can enter an event
+  // add event listener for container div listening to child elements
+  // when clicked, edit class for block border
+  // 
+  
+  
+  // WHEN I click the save button for that time block
+  // THEN the text for that event is saved in local storage
+  // save button is form submit
+  // append to schedule array
+  // save schedule array to local memory
+  
+  
+  // WHEN I refresh the page
+  // THEN the saved events persist
+  // initialize page by pulling storedSchedule from local memory
+
+
+
+
+});
+
+
+
